b61731a29dc16ed11f63a6bb1e452a879aaece26
Dmitry Borodin <borodindr@gmail.com>
2019-06-05 23:24:04 +0300

    Initial Commit

---
+++ Photo Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Photo Search
+//
+//  Created by Dmitry Borodin on 05/06/2019.
+//  Copyright © 2019 Dmitry Borodin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
